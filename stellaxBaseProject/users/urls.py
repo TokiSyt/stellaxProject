142c25b9818c8add6f34a6ea046b28882bbb949c
TokiSyt <yuutokei30@gmail.com>
2025-05-29 13:54:58 +0200

    User personal details change forms

--- stellaxBaseProject/users/urls.py
+++ stellaxBaseProject/users/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("profile/", views.profile_view, name="profile"),
+    path("profile/edit/", views.edit_profile, name="edit_profile"),
+    path("profile/password/", views.change_password, name="change-password"),
     path("settings/", views.user_settings, name="settings"),
 ]
--- stellaxBaseProject/users/views.py
+++ stellaxBaseProject/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
-from .forms import RegisterForm
+from .forms import RegisterForm, EditProfileForm, CustomPasswordChangeForm
 
 
 def register(request):
@@ -10,7 +11,10 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-            new_user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+            new_user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password1"],
+            )
             login(request, new_user)
             return redirect("home")
     else:
@@ -18,7 +22,7 @@ def register(request):
 
     context = {"form": form}
 
-    return render(request, "users/register.html", context)
+    return render(request, "registration/register.html", context)
 
 
 @login_required
@@ -33,3 +37,38 @@ def user_settings(request):
 
     context = {}
     return render(request, "wip.html", context)
+
+
+@login_required
+def edit_profile(request):
+
+    if request.method == "POST":
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:  # GET Post
+        form = EditProfileForm(instance=request.user)
+
+    context = {"form": form}
+    return render(request, "users/edit_profile.html", context)
+
+
+@login_required
+def change_password(request):
+
+    if request.method == "POST":
+        form = CustomPasswordChangeForm(data=request.POST, user=request.user)
+        
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect("profile")
+        else:
+            return redirect("profile/change-password")
+
+    else:
+        form = CustomPasswordChangeForm(user=request.user)
+
+    context = {"form": form}
+    return render(request, "users/change_password.html", context)
