04b4d4329ece974faa34cdab9c38e3392e126935
TokiSyt <yuutokei30@gmail.com>
2025-08-25 13:23:21 +0200

    Initial commit for point system

--- apps/point_system/urls.py
+++ apps/point_system/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, DashboardView
 
-urlpatterns = [path("", HomeView.as_view(), name="home")]
+urlpatterns = [
+    path("", HomeView.as_view(), name="karma-home"),
+    path("karma_dashboard/<int:pk>", DashboardView.as_view(), name="karma-dashboard"),
+]
--- apps/point_system/views.py
+++ apps/point_system/views.py
@@ -1,6 +1,16 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from apps.group_maker.models import GroupCreationModel
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
+    template_name = "point_system/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["groups"] = GroupCreationModel.objects.filter(user=self.request.user)
+        return context
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "wip.html"
