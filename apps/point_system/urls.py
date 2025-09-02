fd5e112ab0887da73055a42fe2a854bfc727b561
TokiSyt <yuutokei30@gmail.com>
2025-09-02 15:38:06 +0200

    Finishing base level for points system app

--- apps/point_system/urls.py
+++ apps/point_system/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import HomeView, DashboardView
+from .views import HomeView, DashboardView, AddColumn, DeleteColumn
+
+app_name = "karma"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="karma-home"),
+    path("new_column/<int:pk>", AddColumn.as_view(), name="new-column"),
+    path("delete_column/<int:pk>", DeleteColumn.as_view(), name="delete-column"),
     path("karma_dashboard/<int:pk>", DashboardView.as_view(), name="karma-dashboard"),
 ]
